8c3c9acd9a6fc204b927a361703ad7502d457b5d
arkangel6 <league@WTS7.attlocal.net>
2018-04-23 19:12:09 -0700

    asdfasd

--- src/interfaces/MixedCapsString.java
+++ src/interfaces/MixedCapsString.java
@@ -12,16 +12,21 @@ public class MixedCapsString extends SpecialString {
 	@Override
 	public String funkifyText(String f) {
 		String lowercase = "";
-		
+		f = f.toLowerCase();
 		char[] letters = f.toCharArray();
 		System.out.println(Arrays.toString(letters));
-		lowercase += (char)(letters[0]+32);
-		for(int i = 1; i < letters.length; i++) {
-			if(i%2 == 1) {
+		//lowercase += (char)(letters[0]+32);
+		for(int i = 0; i < letters.length; i++) {
+			if(letters[i] == ' ' || letters[i] == 0) {
+				lowercase += ' ';
+			}
+			else if(i%2 == 1) {
 				int let = (int)letters[i] - 32;
 				char letter = (char) let;
+				System.out.println(let);
 				lowercase += letter;
 			}else {
+				System.out.println(letters[i]);
 				lowercase += letters[i];
 			}
 			
